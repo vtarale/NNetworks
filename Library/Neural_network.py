738a84fab66ef83926734925950880199e6950ef
Vihaan Tarale <taralevihaan@gmail.com>
2022-04-01 21:10:17 +0530

    completed vectors

--- Library/Neural_network.py
+++ Library/Neural_network.py
@@ -1,2 +1,10 @@
 import Activations
 from Vector import Vector
+
+class Unit:
+    def __init__(self):
+        pass
+
+class NNetwork:
+    def __init__(self):
+        pass
--- Library/Vector.py
+++ Library/Vector.py
@@ -1,3 +1,43 @@
 class Vector:
     def __init__(self, vals: list):
-        pass
+        self.vals = vals
+    # some math operations
+    def add_new(self, vector):
+        new = []
+        for index in range(len(vector)):
+            new.append(self.vals[index] + vector.vals[index])
+        return Vector(new)
+    def sub_new(self, vector):
+        new = []
+        for index in range(len(vector)):
+            new.append(self.vals[index] - vector.vals[index])
+        return Vector(new)
+    def multiply_new(self, vector):
+        new = []
+        for index in range(len(vector)):
+            new.append(self.vals[index] * vector.vals[index])
+        return Vector(new)
+    def div_new(self, vector):
+        new = []
+        for index in range(len(vector)):
+            new.append(self.vals[index] / vector.vals[index])
+        return Vector(new)
+    # some more
+    def add(self, vector):
+        for index in range(len(vector)):
+            self.vals[index] = self.vals[index] + vector.vals[index]
+    def sub(self, vector):
+        for index in range(len(vector)):
+            self.vals[index] = self.vals[index] + vector.vals[index]
+    def multiply(self, vector):
+        for index in range(len(vector)):
+            self.vals[index] = self.vals[index] * vector.vals[index]
+    def div(self, vector):
+        for index in range(len(vector)):
+            self.vals[index] = self.vals[index] / vector.vals[index]
+    # dot product
+    def dot(self, vector):
+        dot = 0
+        for index in range(len(vector)):
+            dot += self.vals[index] * vector.vals[index]
+        return dot
