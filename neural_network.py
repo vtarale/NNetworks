517eb6f0c618a4a0d9ca81a050f37e495b203474
Ashwini Tarale <ashwinitarale@Ashwinis-Mac-mini.local>
2022-09-12 20:16:33 +0530

    good

--- neural_network.py
+++ neural_network.py
@@ -1,16 +1,39 @@
+"""
+Author: vtarale
+"""
+
 # implementing neural networks
+
+
 import random
+import math
 
-# a perceptron
-class Unit:
-    def __init__(self, learning_rate: float, inputs: int):
-        self.learning_rate =learning_rate
-        self.weigths = []
-        print(inputs)
+# setting variables
+inputs_val = 2
+hidden_val = 4
+output_val  = 1
+inputs, ouput, hidden = [], [], []
 
-        for _ in range(inputs):
-            self.weigths.append(random.uniform(0, 3))
+training_data = [[1, 1], [0, 1], [1, 0], [1, 0]]
+answer_data = [0, 1, 1, 0]
 
-        print(self.weigths)
+hidden_weigths = [[random.uniform(0, 3), random.uniform(0, 3)], [random.uniform(0, 3), random.uniform(0, 3)], [random.uniform(0, 3), random.uniform(0, 3)], [random.uniform(0, 3), random.uniform(0, 3)]]
+print(hidden_weigths)
+output_weights = [random.uniform(0, 3), random.uniform(0, 3), random.uniform(0, 3), random.uniform(0, 3)]
 
-dummy_unit = Unit(1, 5)
+bias = 1
+learning_rate = 0.5
+
+
+# some functions
+def activation(x):
+    return 1/(1+math.exp(-x))
+
+def predict(inputs: list):
+    answers = []
+    # y = mx+b
+    for i in range(hidden_val):
+        answers.append(activation((hidden_weigths[i][0] * inputs[0] + hidden_weigths[i][1] * inputs[1]) + bias))
+        print(answers[i])
+
+predict([0, 0])
