48fc74899ae8606fd5c34241122ed2819df8f5f2
Ashwini Tarale <ashwinitarale@Ashwinis-Mac-mini.local>
2022-09-13 21:24:31 +0530

    good

--- neural_network.py
+++ neural_network.py
@@ -20,6 +20,7 @@ answer_data = [0, 1, 1, 0]
 hidden_weigths = [[random.uniform(0, 3), random.uniform(0, 3)], [random.uniform(0, 3), random.uniform(0, 3)], [random.uniform(0, 3), random.uniform(0, 3)], [random.uniform(0, 3), random.uniform(0, 3)]]
 print(hidden_weigths)
 output_weights = [random.uniform(0, 3), random.uniform(0, 3), random.uniform(0, 3), random.uniform(0, 3)]
+print(output_weights)
 
 bias = 1
 learning_rate = 0.5
@@ -35,5 +36,14 @@ def predict(inputs: list):
     for i in range(hidden_val):
         answers.append(activation((hidden_weigths[i][0] * inputs[0] + hidden_weigths[i][1] * inputs[1]) + bias))
         print(answers[i])
+    
+    final_prediction = activation((output_weights[0]*answers[0] + output_weights[1]*answers[1] + output_weights[2]*answers[2] + output_weights[3]*answers[3]) + bias)
+    print(final_prediction)
 
-predict([0, 0])
+    return final_prediction, answers
+
+def update():
+    # calculate answer the backpropogate
+    pass
+
+prediction, _ = predict([0, 0])
